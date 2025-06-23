3a81288b13a974bbc74697d346c79e59b109e23a
sumin2391 <baesumin0211@gmail.com>
2025-06-23 12:04:53 +0900

    250623.12:04 - MS_ 관련 몬스터 헤더 비활성화 및 /Monster_Factroy.h 로 병합

--- TestRPGteam/TestRPGteam/MS_Golem.h
+++ TestRPGteam/TestRPGteam/MS_Golem.h
@@ -1,17 +1,17 @@
-#pragma once
-#include "Monster.h"
-#include <string>
-
-class Golem : public Monster {
-public:
-
-	// - 메인쪽에서 '레벨 정수값 넣어"줘" 입니다.'
-	// - 체력: (레벨 × 50)
-	// - 공격력 : (레벨 × 15)
-	Golem(int level) {
-		MonsterName = "덩치만 큰 골렘";
-		MonsterHealth = level * 50;
-		MonsterAttack = level * 15;
-		Experience = (level * 3) / 2;
-	}
-};
+//#pragma once
+//#include "Monster.h"
+//#include <string>
+//
+//class Golem : public Monster {
+//public:
+//
+//	// - 메인쪽에서 '레벨 정수값 넣어"줘" 입니다.'
+//	// - 체력: (레벨 × 50)
+//	// - 공격력 : (레벨 × 15)
+//	Golem(int level) {
+//		MonsterName = "덩치만 큰 골렘";
+//		MonsterHealth = level * 50;
+//		MonsterAttack = level * 15;
+//		Experience = (level * 3) / 2;
+//	}
+//};
