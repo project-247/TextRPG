915cea69eb038efb77737dd1e4a1ef73c225c369
n-fia <78014168+n-fia@users.noreply.github.com>
2025-06-24 20:23:49 +0900

    원복원복

--- TextRPG/monsters/MS_BossDragon.h
+++ TextRPG/monsters/MS_BossDragon.h
@@ -6,8 +6,8 @@ public:
 	BossDragon(int level) {
 		MonsterName = "보스 드래곤";
 		//형변환 방지를 위해 1.5 >> 3/2로 변경
-		MonsterHealth = level * 500* 1.5;
-		MonsterAttack = level * 50* 1.5;
+		MonsterHealth = level * 500 * 1.5;
+		MonsterAttack = level * 50 * 1.5;
 		Experience = 9999;
 	}
 };
