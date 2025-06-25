00f3a0bc2a6c7134329da855fe33aef65da6b514
247 <malo93@naver.com>
2025-06-25 09:19:01 +0900

    Update MS_BossDragon.h

--- TextRPG/monsters/MS_BossDragon.h
+++ TextRPG/monsters/MS_BossDragon.h
@@ -5,14 +5,9 @@ class BossDragon : public Monster {
 public:
 	BossDragon(int level) {
 		MonsterName = "보스 드래곤";
-
-		// 체력: (레벨×20×1.5) ~ (레벨×30×1.5)
-		int minHP = static_cast<int>(level * 20 * 1.5);
-		int maxHP = static_cast<int>(level * 30 * 1.5);
-		MonsterHealth = rand() % (maxHP - minHP + 1) + minHP;
-
-		// 공격력: (레벨×5×1.5) ~ (레벨×10×1.5)
-		int minAttack = static_cast<int>(level * 5 * 1.5);
-		int maxAttack = static_cast<int>(level * 10 * 1.5);
-		MonsterAttack = rand() % (maxAttack - minAttack + 1) + minAttack;
-	};
+		//형변환 방지를 위해 1.5 >> 3/2로 변경
+		MonsterHealth = level * 500 * 3 / 2;
+		MonsterAttack = level * 50 * 3 / 2;
+		Experience = 9999;
+	}
+};
