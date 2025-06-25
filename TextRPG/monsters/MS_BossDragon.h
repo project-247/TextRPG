256696236f61faa49cde3ca16023117f6ef44289
sumin2391 <baesumin0211@gmail.com>
2025-06-25 14:27:29 +0900

    .

--- TextRPG/monsters/MS_BossDragon.h
+++ TextRPG/monsters/MS_BossDragon.h
@@ -5,8 +5,8 @@ class BossDragon : public Monster {
 public:
 	BossDragon(int level) {
 		// 체력: (레벨×20×1.5) ~ (레벨×30×1.5)More actions
-		int minHP = level * 30;
-		int maxHP = level * 45;
+		int minHP = level * 200;
+		int maxHP = level * 400;
 		MonsterHealth = rand() % (maxHP - minHP + 1) + minHP;
 
 		// 공격력: (레벨×5×1.5) ~ (레벨×10×1.5)
